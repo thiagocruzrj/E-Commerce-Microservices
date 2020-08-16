0faa6d06c2fee58b26f2c41ae8059acdbb3cb45f
Thiago Cruz <thagocruz@gmail.com>
2020-08-16 17:11:04 -0300

    Create CatalogContextSeed.cs

---
+++ MSStore/src/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public static class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+
+        }
+    }
+}
