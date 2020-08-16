14d2ae7fefc074409ff535d870466f9c27f6a849
Thiago Cruz <thagocruz@gmail.com>
2020-08-16 16:57:55 -0300

    Add DI files

--- MSStore/src/Catalog.API/Startup.cs
+++ MSStore/src/Catalog.API/Startup.cs
@@ -1,8 +1,10 @@
+using Catalog.API.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Catalog.API
 {
@@ -18,6 +20,8 @@ namespace Catalog.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.Configure<CatalogDbSettings>(Configuration.GetSection(nameof(CatalogDbSettings)));
+            services.AddSingleton<ICatalogDbSettings>(sp => sp.GetRequiredService<IOptions<CatalogDbSettings>>().Value);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
