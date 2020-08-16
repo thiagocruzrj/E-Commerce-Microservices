cf029eacfb9e702c4cd62aecc7def6125b9090b3
Thiago Cruz <thagocruz@gmail.com>
2020-08-16 15:41:18 -0300

    Add MongoDb on project

--- MSStore/src/Catalog.API/Entities/Product.cs
+++ MSStore/src/Catalog.API/Entities/Product.cs
@@ -1,8 +1,15 @@
-﻿namespace Catalog.API.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
 {
     public class Product
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
+        [BsonElement("Name")]
         public string Name { get; set; }
         public string Category { get; set; }
         public string Summary { get; set; }
