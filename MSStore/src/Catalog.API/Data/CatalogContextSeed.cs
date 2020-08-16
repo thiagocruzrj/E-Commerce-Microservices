77f1c3f109418af1c45d0f3e30d8457f98341cb8
Thiago Cruz <thagocruz@gmail.com>
2020-08-16 17:17:03 -0300

    Create some products

--- MSStore/src/Catalog.API/Data/CatalogContextSeed.cs
+++ MSStore/src/Catalog.API/Data/CatalogContextSeed.cs
@@ -1,5 +1,7 @@
 ﻿using Catalog.API.Entities;
 using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
 
 namespace Catalog.API.Data
 {
@@ -7,7 +9,50 @@ namespace Catalog.API.Data
     {
         public static void SeedData(IMongoCollection<Product> productCollection)
         {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if(!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
 
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Samsung Note 10 Plus",
+                    Summary = "Test Summary",
+                    Description = "Test Description",
+                    Price = 600.00M,
+                    Category = "Smartphone"
+                },
+                new Product()
+                {
+                    Name = "Samsung Note 10",
+                    Summary = "Test Summary",
+                    Description = "Test Description",
+                    Price = 500.00M,
+                    Category = "Smartphone"
+                },
+                new Product()
+                {
+                    Name = "Samsung S10 Plus",
+                    Summary = "Test Summary",
+                    Description = "Test Description",
+                    Price = 450.00M,
+                    Category = "Smartphone"
+                },
+                new Product()
+                {
+                    Name = "Iphone X",
+                    Summary = "Test Summary",
+                    Description = "Test Description",
+                    Price = 700.00M,
+                    Category = "Smartphone"
+                },
+            };
         }
     }
 }
