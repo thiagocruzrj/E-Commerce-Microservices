af95cf771634b0c478ef0626bfd202b1b51062ad
Thiago Cruz <thagocruz@gmail.com>
2020-08-16 16:57:31 -0300

    Add connection settings

---
+++ MSStore/src/Catalog.API/Settings/CatalogDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Settings
+{
+    public class CatalogDbSettings : ICatalogDbSettings
+    {
+        public string DatabaseName { get; set; }
+        public string ConnectionString { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
