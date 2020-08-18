b230c98a2cfc77091c75b1bcd812708468d31526
Thiago Cruz <thagocruz@gmail.com>
2020-08-17 23:35:18 -0300

    Add DI life cycle on startup

--- MSStore/src/Catalog.API/Startup.cs
+++ MSStore/src/Catalog.API/Startup.cs
@@ -1,5 +1,7 @@
 using Catalog.API.Data;
 using Catalog.API.Data.Interfaces;
+using Catalog.API.Repositories;
+using Catalog.API.Repositories.Interfaces;
 using Catalog.API.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -26,6 +28,7 @@ namespace Catalog.API
             services.AddSingleton<ICatalogDbSettings>(sp => sp.GetRequiredService<IOptions<CatalogDbSettings>>().Value);
 
             services.AddTransient<ICatalogContext, CatalogContext>();
+            services.AddTransient<IProductRepository, ProductRepositoty>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
