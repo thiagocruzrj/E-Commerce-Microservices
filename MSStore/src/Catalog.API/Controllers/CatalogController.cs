506c30cbc9fd39a296d67ea5c44608abec54d9cb
Thiago Cruz <thagocruz@gmail.com>
2020-08-17 23:38:13 -0300

    Add IProductRepository injection on controller

--- MSStore/src/Catalog.API/Controllers/CatalogController.cs
+++ MSStore/src/Catalog.API/Controllers/CatalogController.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catalog.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Catalog.API.Controllers
 {
     public class CatalogController : ControllerBase
     {
+        private readonly IProductRepository _productRepository;
+
+        public CatalogController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new ArgumentException(nameof(productRepository));
+        }
     }
 }
