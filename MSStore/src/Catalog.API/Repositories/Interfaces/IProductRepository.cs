1f492f3ae963cfc8200c4b8ed4c7add85c2c55a4
Thiago Cruz <thagocruz@gmail.com>
2020-08-17 22:35:56 -0300

    Create IProductRepository.cs

---
+++ MSStore/src/Catalog.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+    }
+}
