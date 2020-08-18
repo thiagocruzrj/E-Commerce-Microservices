48cea2e746eaf9791ab40c2c33acfde731079db8
Thiago Cruz <thagocruz@gmail.com>
2020-08-18 00:11:41 -0300

    Fix id length and route

--- MSStore/src/Catalog.API/Controllers/CatalogController.cs
+++ MSStore/src/Catalog.API/Controllers/CatalogController.cs
@@ -31,7 +31,7 @@ namespace Catalog.API.Controllers
             return Ok(products);
         }
 
-        [HttpGet]
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
         [ProducesResponseType((int)HttpStatusCode.NotFound)]
         [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<Product>> GetProductById(string id)
