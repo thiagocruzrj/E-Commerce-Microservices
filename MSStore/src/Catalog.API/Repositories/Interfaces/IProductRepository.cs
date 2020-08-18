a00ffe045ed7e7bc981bc833729232477ad1e158
Thiago Cruz <thagocruz@gmail.com>
2020-08-17 22:59:11 -0300

    Add ProductRepositoty class

--- MSStore/src/Catalog.API/Repositories/Interfaces/IProductRepository.cs
+++ MSStore/src/Catalog.API/Repositories/Interfaces/IProductRepository.cs
@@ -10,6 +10,7 @@ namespace Catalog.API.Repositories.Interfaces
         Task<Product> GetProduct(string id);
         Task<IEnumerable<Product>> GetProductByName(string name);
         Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+
         Task Create(Product product);
         Task<bool> Update(Product product);
         Task<bool> Delete(Product product);
---
+++ MSStore/src/Catalog.API/Repositories/ProductRepositoty.cs
@@ -0,0 +1,46 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepositoty : IProductRepository
+    {
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
