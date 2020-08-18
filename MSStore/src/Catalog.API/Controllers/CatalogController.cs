c48591abae3ffd32a598097c97a6f8266d3e704c
Thiago Cruz <thagocruz@gmail.com>
2020-08-17 23:51:04 -0300

    Add GetProducts endpoint

--- MSStore/src/Catalog.API/Controllers/CatalogController.cs
+++ MSStore/src/Catalog.API/Controllers/CatalogController.cs
@@ -1,9 +1,15 @@
-﻿using Catalog.API.Repositories.Interfaces;
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Catalog.API.Controllers
 {
+    [Route("api/v1/[controller]")]
+    [ApiController]
     public class CatalogController : ControllerBase
     {
         private readonly IProductRepository _productRepository;
@@ -11,6 +17,14 @@ namespace Catalog.API.Controllers
         public CatalogController(IProductRepository productRepository)
         {
             _productRepository = productRepository ?? throw new ArgumentException(nameof(productRepository));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
         }
     }
 }
