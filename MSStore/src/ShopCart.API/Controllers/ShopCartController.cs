a0957f0171b74d7fa4c909bd437095664eed04fd
Thiago Cruz <thagocruz@gmail.com>
2020-08-19 17:05:42 -0300

    Add Get shop cart endpoint

--- MSStore/src/ShopCart.API/Controllers/ShopCartController.cs
+++ MSStore/src/ShopCart.API/Controllers/ShopCartController.cs
@@ -1,10 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopCart.API.Entities;
+using ShopCart.API.Repositories.Interfaces;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace ShopCart.API.Controllers
 {
-    [ApiController]
     [Route("api/v1/[controller]")]
+    [ApiController]
     public class ShopCartController : ControllerBase
     {
+        private readonly IShopCartRepository _shopCartRepository;
+
+        public ShopCartController(IShopCartRepository shopCartRepository)
+        {
+            _shopCartRepository = shopCartRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetShopCart(string userName)
+        {
+            var shopCart = await _shopCartRepository.GetShopCart(userName);
+            return Ok(shopCart);
+        }
     }
 }
