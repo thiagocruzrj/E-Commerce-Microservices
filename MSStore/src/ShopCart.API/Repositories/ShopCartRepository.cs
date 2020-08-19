eabac20c2b19af28efcbe2bbaf9755196178294d
Thiago Cruz <thagocruz@gmail.com>
2020-08-19 16:33:30 -0300

    Create ShopCartRepository.cs

---
+++ MSStore/src/ShopCart.API/Repositories/ShopCartRepository.cs
@@ -0,0 +1,25 @@
+﻿using ShopCart.API.Entities;
+using ShopCart.API.Repositories.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace ShopCart.API.Repositories
+{
+    public class ShopCartRepository : IShopCartRepository
+    {
+        public Task<ShoppingCart> GetShopCart(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> UpdateShopCart(ShoppingCart shopCart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteShopCart(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
