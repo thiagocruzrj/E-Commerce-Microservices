64caf6ddb30ab3074696744c8a75a82eadda889c
Thiago Cruz <thagocruz@gmail.com>
2020-08-19 16:32:27 -0300

    Create IShopCartRepository.cs

---
+++ MSStore/src/ShopCart.API/Repositories/Interfaces/IShopCartRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShopCart.API.Entities;
+using System.Threading.Tasks;
+
+namespace ShopCart.API.Repositories.Interfaces
+{
+    public interface IShopCartRepository
+    {
+        Task<ShoppingCart> GetShopCart(string userName);
+        Task<ShoppingCart> UpdateShopCart(ShoppingCart shopCart);
+        Task<bool> DeleteShopCart(string userName);
+    }
+}
