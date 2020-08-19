0d3af005439e4db6912dfbaa5bf4553d4ed9f80c
Thiago Cruz <thagocruz@gmail.com>
2020-08-19 16:01:24 -0300

    Create ShopCartContext.cs

---
+++ MSStore/src/ShopCart.API/Data/ShopCartContext.cs
@@ -0,0 +1,19 @@
+﻿using ShopCart.API.Data.Interfaces;
+using StackExchange.Redis;
+using System;
+
+namespace ShopCart.API.Data
+{
+    public class ShopCartContext : IShopCartContext
+    {
+        private readonly IConnectionMultiplexer _redisConnection;
+
+        public ShopCartContext(IConnectionMultiplexer redisConnection)
+        {
+            _redisConnection = redisConnection;
+            Redis = _redisConnection.GetDatabase();
+        }
+
+        public IDatabase Redis { get; }
+    }
+}
