c8795b4a4362ba28538d1dd9e87f824eac5e48df
Thiago Cruz <thagocruz@gmail.com>
2020-08-19 15:36:59 -0300

    Create ShopCartItem.cs

---
+++ MSStore/src/ShopCart.API/Entities/ShopCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopCart.API.Entities
+{
+    public class ShopCartItem
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+    }
+}
