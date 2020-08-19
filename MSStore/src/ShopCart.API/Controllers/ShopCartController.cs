9a125c4eb63b6cdc6ba46424dfcea46e4bad054c
Thiago Cruz <thagocruz@gmail.com>
2020-08-19 15:28:47 -0300

    Add shopCart base controller

---
+++ MSStore/src/ShopCart.API/Controllers/ShopCartController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopCart.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ShopCartController : ControllerBase
+    {
+    }
+}
