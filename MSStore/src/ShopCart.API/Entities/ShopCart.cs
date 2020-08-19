5437b3bf39c374bdb389f0a0cd1a0017984b449c
Thiago Cruz <thagocruz@gmail.com>
2020-08-19 15:36:55 -0300

    Create ShopCart.cs

---
+++ MSStore/src/ShopCart.API/Entities/ShopCart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ShopCart.API.Entities
+{
+    public class ShopCart
+    {
+        public string UserName { get; set; }
+        public List<ShopCartItem> Item { get; set; } = new List<ShopCartItem>();
+
+        public ShopCart() { }
+
+        public ShopCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        // Calculate total price shop cart
+    }
+}
