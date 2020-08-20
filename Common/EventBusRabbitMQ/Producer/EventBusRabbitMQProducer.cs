dcb420761eede2bdbbcffe1e574e444706d3e31c
Thiago Cruz <thagocruz@gmail.com>
2020-08-20 12:02:54 -0300

    Add message and body message

--- Common/EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
+++ Common/EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EventBusRabbitMQ.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace EventBusRabbitMQ.Producer
 {
@@ -9,6 +13,16 @@ namespace EventBusRabbitMQ.Producer
         public EventBusRabbitMQProducer(IRabbitMQConnection connection)
         {
             _connection = connection ?? throw new ArgumentException(nameof(connection));
+        }
+
+        public void PublishShopCartCheckout(string queueName, ShopCartCheckouEvent publishModel)
+        {
+            using(var channel = _connection.CreateModel())
+            {
+                channel.QueueDeclare(queueName, false, false, false, null);
+                var message = JsonConvert.SerializeObject(publishModel);
+                var body = Encoding.UTF8.GetBytes(message);
+            }
         }
     }
 }
