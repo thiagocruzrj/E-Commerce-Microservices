efc5bcf0d5d0c9bb251f2d8179e9e14dcc3eb7d9
Thiago Cruz <thagocruz@gmail.com>
2020-08-20 11:50:49 -0300

    Create EventBusRabbitMQProducer.cs

---
+++ Common/EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EventBusRabbitMQ.Producer
+{
+    public class EventBusRabbitMQProducer
+    {
+        private readonly IRabbitMQConnection _connection;
+
+        public EventBusRabbitMQProducer(IRabbitMQConnection connection)
+        {
+            _connection = connection ?? throw new ArgumentException(nameof(connection));
+        }
+    }
+}
