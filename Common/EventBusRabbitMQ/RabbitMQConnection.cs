0ab242205b4d995f9d3a6b32e26ef8d345881688
Thiago Cruz <thagocruz@gmail.com>
2020-08-20 11:12:52 -0300

    Create RabbitMQConnection.cs

---
+++ Common/EventBusRabbitMQ/RabbitMQConnection.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+
+namespace EventBusRabbitMQ
+{
+    public class RabbitMQConnection : IRabbitMQConnection
+    {
+        public bool IsConnected => throw new System.NotImplementedException();
+
+        public IModel CreateModel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryConnect()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
