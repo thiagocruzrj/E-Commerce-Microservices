1412a521bf15bd32f729ef3d43622cc5846c1226
Thiago Cruz <thagocruz@gmail.com>
2020-08-20 11:48:44 -0300

    Create ShopCartCheckouEvent.cs

---
+++ Common/EventBusRabbitMQ/Events/ShopCartCheckouEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EventBusRabbitMQ.Events
+{
+    public class ShopCartCheckouEvent
+    {
+        public Guid RequestId { get; set; }
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        // BillingAddress
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string AddressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        // Payment
+        public string CartName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
