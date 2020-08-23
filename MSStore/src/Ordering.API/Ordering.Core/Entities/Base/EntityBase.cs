b4deb60e6cdf003d6397295d0729c521a1e47138
Thiago Cruz <thagocruz@gmail.com>
2020-08-23 12:04:28 -0300

    Override equals method

--- MSStore/src/Ordering.API/Ordering.Core/Entities/Base/EntityBase.cs
+++ MSStore/src/Ordering.API/Ordering.Core/Entities/Base/EntityBase.cs
@@ -10,5 +10,24 @@
         {
             return Id.Equals(default(TId));
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is EntityBase<TId>))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            var item = (EntityBase<TId>)obj;
+
+            if (item.IsTransient() || IsTransient())
+                return false;
+            else
+                return item == this;
+        }
     }
 }
