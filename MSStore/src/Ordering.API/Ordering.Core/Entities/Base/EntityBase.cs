9d23b0a13c454592d58005e13977b4a2c76b2f22
Thiago Cruz <thagocruz@gmail.com>
2020-08-23 12:02:13 -0300

    Create EntityBase.cs

---
+++ MSStore/src/Ordering.API/Ordering.Core/Entities/Base/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Core.Entities.Base
+{
+    public abstract class EntityBase<TId> : IEntityBase<TId>
+    {
+        public virtual TId Id{ get; protected set; }
+
+        int? _requestedHashCode;
+
+        public bool IsTransient()
+        {
+            return Id.Equals(default(TId));
+        }
+    }
+}
