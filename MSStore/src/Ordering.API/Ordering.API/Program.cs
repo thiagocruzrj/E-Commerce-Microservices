0aa132c789d75255820a2b3e0ef9681e2c6f06a3
Thiago Cruz <thagocruz@gmail.com>
2020-08-24 11:57:44 -0300

    Add CreateAndSeedDatabase on pgm class to execulte migration automatically

--- MSStore/src/Ordering.API/Ordering.API/Program.cs
+++ MSStore/src/Ordering.API/Ordering.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ordering.Infra.Data;
+using System;
 
 namespace Ordering.API
 {
@@ -7,7 +11,9 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateAndSeedDatabase(host);
+            host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -16,5 +22,25 @@ namespace Ordering.API
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateAndSeedDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var orderContext = services.GetRequiredService<OrderContext>();
+                    OrderContextSeed.SeedAsync(orderContext, loggerFactory);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex.Message);
+                }
+            }
+        }
     }
 }
