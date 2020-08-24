82128d11efbaafe66e3b15d26456362589e9c73a
Thiago Cruz <thagocruz@gmail.com>
2020-08-24 11:56:59 -0300

    Add referemces

--- MSStore/src/Ordering.API/Ordering.Infra/Data/OrderContextSeed.cs
+++ MSStore/src/Ordering.API/Ordering.Infra/Data/OrderContextSeed.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.Logging;
 using Ordering.Core.Entities;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
