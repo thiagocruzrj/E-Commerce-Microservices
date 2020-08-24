f2791e52b87166afc1efa0e519e13eda2d472be7
Thiago Cruz <thagocruz@gmail.com>
2020-08-24 11:27:02 -0300

    Create OrderContext.cs

---
+++ MSStore/src/Ordering.API/Ordering.Infra/Data/OrderContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infra.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
