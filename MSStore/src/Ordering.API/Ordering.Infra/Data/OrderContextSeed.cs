61e79b8508b0a117d4a329b6bb9aea69ce904cc2
Thiago Cruz <thagocruz@gmail.com>
2020-08-24 11:43:29 -0300

    Add SeedAsync on OrderContextSeed

---
+++ MSStore/src/Ordering.API/Ordering.Infra/Data/OrderContextSeed.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Ordering.Infra.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            try
+            {
+                orderContext.Database.Migrate();
+
+                if(!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                if(retryForAvailability < 3)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<OrderContextSeed>();
+                    log.LogError(ex.Message, loggerFactory, retryForAvailability);
+                }
+                throw;
+            }
+        }
+    }
+}
