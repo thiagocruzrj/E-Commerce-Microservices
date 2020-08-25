98699858096e252172412e5c38041402b28dac56
Thiago Cruz <thagocruz@gmail.com>
2020-08-25 14:58:36 -0300

    Create OrderMapper.cs

---
+++ MSStore/src/Ordering.API/Ordering.Application/Mapper/OrderMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+
+namespace Ordering.Application.Mapper
+{
+    public static class OrderMapper
+    {
+        private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<OrderMappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => Lazy.Value;
+    }
+}
