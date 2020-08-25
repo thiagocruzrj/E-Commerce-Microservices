a2b99ac4c348917ad51a415638e2d36add7299a8
Thiago Cruz <thagocruz@gmail.com>
2020-08-25 14:45:56 -0300

    Add prop on ctor

--- MSStore/src/Ordering.API/Ordering.Application/Queries/GetOrderByUseerNameQuery.cs
+++ MSStore/src/Ordering.API/Ordering.Application/Queries/GetOrderByUseerNameQuery.cs
@@ -1,9 +1,17 @@
 ﻿using MediatR;
+using Ordering.Application.Responses;
+using System;
 using System.Collections.Generic;
 
 namespace Ordering.Application.Queries
 {
     public class GetOrderByUseerNameQuery : IRequest<IEnumerable<OrderResponse>>
     {
+        public string UserName { get; set; }
+
+        public GetOrderByUseerNameQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentException(nameof(userName));
+        }
     }
 }
