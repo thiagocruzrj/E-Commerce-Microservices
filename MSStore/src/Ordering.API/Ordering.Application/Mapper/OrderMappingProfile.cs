8dd755a16de72ecfed39bd56eb6bc0d066ff45ae
Thiago Cruz <thagocruz@gmail.com>
2020-08-25 15:00:13 -0300

    Create OrderMappingProfile.cs

---
+++ MSStore/src/Ordering.API/Ordering.Application/Mapper/OrderMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+
+namespace Ordering.Application.Mapper
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            CreateMap<Order, OrderResponse>().ReverseMap();
+        }
+    }
+}
