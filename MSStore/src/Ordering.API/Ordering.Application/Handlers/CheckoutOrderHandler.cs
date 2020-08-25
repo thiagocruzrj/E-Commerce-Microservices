09e12cb64582abd3126b3f705b81305f079cca98
Thiago Cruz <thagocruz@gmail.com>
2020-08-25 15:09:48 -0300

    Create CheckoutOrderHandler.cs

---
+++ MSStore/src/Ordering.API/Ordering.Application/Handlers/CheckoutOrderHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Ordering.Application.Commands;
+using Ordering.Application.Responses;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Handlers
+{
+    public class CheckoutOrderHandler : IRequestHandler<CheckoutOrderCommand, OrderResponse>
+    {
+        public Task<OrderResponse> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
