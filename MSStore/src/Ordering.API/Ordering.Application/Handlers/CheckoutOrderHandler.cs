f0fded498344d23ba34c29b6de7e81e7885bd3ff
Thiago Cruz <thagocruz@gmail.com>
2020-08-25 15:10:55 -0300

    Injecting IOrderRepository

--- MSStore/src/Ordering.API/Ordering.Application/Handlers/CheckoutOrderHandler.cs
+++ MSStore/src/Ordering.API/Ordering.Application/Handlers/CheckoutOrderHandler.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Ordering.Application.Commands;
 using Ordering.Application.Responses;
+using Ordering.Core.Repositories;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,6 +10,13 @@ namespace Ordering.Application.Handlers
 {
     public class CheckoutOrderHandler : IRequestHandler<CheckoutOrderCommand, OrderResponse>
     {
+        private readonly IOrderRepository _orderRepository;
+
+        public CheckoutOrderHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentException(nameof(orderRepository));
+        }
+
         public Task<OrderResponse> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
         {
             throw new System.NotImplementedException();
