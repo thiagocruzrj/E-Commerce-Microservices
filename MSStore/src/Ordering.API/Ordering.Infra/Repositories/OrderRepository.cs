77373aaa0dbf6adbd7ca232760281cd67271c794
Thiago Cruz <thagocruz@gmail.com>
2020-08-25 16:08:59 -0300

    Create OrderRepository.cs

---
+++ MSStore/src/Ordering.API/Ordering.Infra/Repositories/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infra.Data;
+using Ordering.Infra.Repositories.Base;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infra.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext) { }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                      .Where(o => o.UserName == userName)
+                      .ToListAsync();
+
+            return orderList;
+        }
+    }
+}
