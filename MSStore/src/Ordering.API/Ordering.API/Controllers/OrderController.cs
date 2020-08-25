bbcdce71bf1a199522fb5ea841f32ac60423aef4
Thiago Cruz <thagocruz@gmail.com>
2020-08-25 15:57:27 -0300

    Implemente CheckoutOrder endpoint

--- MSStore/src/Ordering.API/Ordering.API/Controllers/OrderController.cs
+++ MSStore/src/Ordering.API/Ordering.API/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Commands;
 using Ordering.Application.Queries;
 using Ordering.Application.Responses;
 using System;
@@ -28,6 +29,15 @@ namespace Ordering.API.Controllers
             var orders = await _mediator.Send(query);
 
             return Ok(orders);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(OrderResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<OrderResponse>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
         }
     }
 }
