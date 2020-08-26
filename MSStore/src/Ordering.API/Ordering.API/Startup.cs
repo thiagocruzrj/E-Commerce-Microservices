20e04767fb3cc8de603988a05e971523aecf67d8
Thiago Cruz <thagocruz@gmail.com>
2020-08-25 21:02:34 -0300

    Update Startup.cs

--- MSStore/src/Ordering.API/Ordering.API/Startup.cs
+++ MSStore/src/Ordering.API/Ordering.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Ordering.API.Extensions;
 using Ordering.API.RabbitMQ;
 using Ordering.Application.Handlers;
 using Ordering.Core.Repositories;
