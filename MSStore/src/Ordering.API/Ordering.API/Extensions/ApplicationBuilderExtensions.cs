749539f40aa21dde85ae666ad4a7f317a5445446
Thiago Cruz <thagocruz@gmail.com>
2020-08-25 21:01:15 -0300

    Update ApplicationBuilderExtensions.cs

--- MSStore/src/Ordering.API/Ordering.API/Extensions/ApplicationBuilderExtensions.cs
+++ MSStore/src/Ordering.API/Ordering.API/Extensions/ApplicationBuilderExtensions.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.API.RabbitMQ;
 
 namespace Ordering.API.Extensions
 {
     public static class ApplicationBuilderExtensions
     {
-        public static IApplicationBuilder UseRabbitListener(this IApplicationBuilder app)
+        public static EventBusRabbitMQConsumer Listener { get; set; }
+
+        public static void UseRabbitListener(this IApplicationBuilder app)
+        {
+            Listener = app.ApplicationServices.GetService<EventBusRabbitMQConsumer>();
+            var lifeCycle = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+            lifeCycle.ApplicationStarted.Register(OnStarted);
+            lifeCycle.ApplicationStopping.Register(OnStopping);
+        }
+
+        private static void OnStarted()
+        {
+
+        }
+
+        private static void OnStopping()
         {
 
         }
