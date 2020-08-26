79bf8ceb58e599522f868c1e6b0f0e78e7997a6b
Thiago Cruz <thagocruz@gmail.com>
2020-08-26 14:51:34 -0300

    Update Startup.cs

--- MSStore/src/APIGateway/Startup.cs
+++ MSStore/src/APIGateway/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
@@ -26,10 +25,7 @@ namespace APIGateway
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
 
             await app.UseOcelot();
