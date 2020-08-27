746de1e947c31fc00e92c92263fcef025507f961
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 18:04:38 -0300

    Create ShopCartApi.cs

---
+++ MSStore/src/WebApp/AspnetRunBasics/ApiCollection/ShopCartApi.cs
@@ -0,0 +1,34 @@
+﻿using AspnetRunBasics.ApiCollection.Infra;
+using AspnetRunBasics.ApiCollection.Interfaces;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Settings;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection
+{
+    public class ShopCartApi : BaseHttpClientWithFactory, IShopCartApi
+    {
+        private readonly IApiSettings _settings;
+
+        public ShopCartApi(IHttpClientFactory factory, IApiSettings settings) : base(factory)
+        {
+            _settings = settings;
+        }
+
+        public Task<ShopCartModel> GetShopCart(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ShopCartModel> UpdateShopCart(ShopCartModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task CheckoutShopCart(ShopCartCheckoutModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
