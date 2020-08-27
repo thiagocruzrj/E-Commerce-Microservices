8099e4f73e57505903ea1aaceba4247afd532a58
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 17:36:13 -0300

    Create ShopCartModel.cs

---
+++ MSStore/src/WebApp/AspnetRunBasics/Models/ShopCartModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AspnetRunBasics.Models
+{
+    public class ShopCartModel
+    {
+        public string UserName { get; set; }
+        public List<ShopCartItemModel> Items { get; set; } = new List<ShopCartItemModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
