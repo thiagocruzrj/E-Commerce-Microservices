d984c866bb5c4e205d2428339bf0201129fee961
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 16:43:13 -0300

    Create BaseHttpClientWithFactory.cs

---
+++ MSStore/src/WebApp/AspnetRunBasics/ApiCollection/Infra/BaseHttpClientWithFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AspnetRunBasics.ApiCollection.Infra
+{
+    public class BaseHttpClientWithFactory
+    {
+        private readonly IHttpClientFactory _factory;
+
+        public Uri BaseAddress { get; set; }
+        public string BasePath { get; set; }
+
+        public BaseHttpClientWithFactory(IHttpClientFactory factory) => _factory = factory;
+
+        private HttpClient GetHttpClient()
+        {
+            return _factory.CreateClient();
+        }
+    }
+}
