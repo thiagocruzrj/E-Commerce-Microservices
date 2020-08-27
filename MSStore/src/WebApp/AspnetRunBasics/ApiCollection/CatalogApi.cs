3b6177a857c7245a9e1b04a5a477fcb5ae8dfc65
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 17:50:07 -0300

    Create CatalogApi.cs

---
+++ MSStore/src/WebApp/AspnetRunBasics/ApiCollection/CatalogApi.cs
@@ -0,0 +1,40 @@
+﻿using AspnetRunBasics.ApiCollection.Infra;
+using AspnetRunBasics.ApiCollection.Interfaces;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Settings;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection
+{
+    public class CatalogApi : BaseHttpClientWithFactory, ICatalogApi
+    {
+        private readonly IApiSettings _settings;
+
+        public CatalogApi(IHttpClientFactory factory, IApiSettings settings) : base(factory)
+        {
+            _settings = settings;
+        }
+
+        public Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CatalogModel> GetCatalogById(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CatalogModel> CreateCatalog(CatalogModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
