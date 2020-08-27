7833014360c703fbbbb2db96dfb1c0e9703a0fde
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 18:16:50 -0300

    Create OrderApi.cs

---
+++ MSStore/src/WebApp/AspnetRunBasics/ApiCollection/OrderApi.cs
@@ -0,0 +1,25 @@
+﻿using AspnetRunBasics.ApiCollection.Infra;
+using AspnetRunBasics.ApiCollection.Interfaces;
+using AspnetRunBasics.Models;
+using AspnetRunBasics.Settings;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection
+{
+    public class OrderApi : BaseHttpClientWithFactory, IOrderApi
+    {
+        private readonly IApiSettings _settings;
+
+        public OrderApi(IHttpClientFactory factory, IApiSettings settings) : base(factory)
+        {
+            _settings = settings;
+        }
+
+        public Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
