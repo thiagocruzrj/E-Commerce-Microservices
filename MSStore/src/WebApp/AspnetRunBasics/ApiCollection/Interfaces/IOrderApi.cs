809dcac13cb5b2848b4182cff1e8c18863cd25c8
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 17:31:28 -0300

    Create IOrderApi.cs

---
+++ MSStore/src/WebApp/AspnetRunBasics/ApiCollection/Interfaces/IOrderApi.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Interfaces
+{
+    interface IOrderApi
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
