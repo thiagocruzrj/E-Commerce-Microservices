5ce9f5066aa2f4b833c035359d9f3fb97693b734
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 17:32:22 -0300

    Create ShopCartApi.cs

---
+++ MSStore/src/WebApp/AspnetRunBasics/ApiCollection/Interfaces/ShopCartApi.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Interfaces
+{
+    interface ShopCartApi
+    {
+        Task<ShopCartModel> GetShopCart(string userName);
+        Task<ShopCartModel> UpdateShopCart(ShopCartModel model);
+        Task CheckoutShopCart(ShopCartCheckoutModel model);
+    }
+}
