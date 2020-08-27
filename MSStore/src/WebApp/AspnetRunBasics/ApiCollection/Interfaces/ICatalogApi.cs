6a8433ae6105439c1cc80f75b3816d3c9f108340
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 17:30:57 -0300

    Create ICatalogApi.cs

---
+++ MSStore/src/WebApp/AspnetRunBasics/ApiCollection/Interfaces/ICatalogApi.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.ApiCollection.Interfaces
+{
+    public interface ICatalogApi
+    {
+        Task<IEnumerable<CatalogModel>> GetCatalog();
+        Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category);
+        Task<CaltalogModel> GetCatalogById(string id);
+        Task<CatalogModel> CreateCatalog(CatalogModel model);
+    }
+}
