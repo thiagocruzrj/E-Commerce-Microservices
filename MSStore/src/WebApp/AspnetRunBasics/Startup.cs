58f5677e2400f37874eb243afc9a28ebf64f1b0a
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 22:04:57 -0300

    Update Startup.cs

--- MSStore/src/WebApp/AspnetRunBasics/Startup.cs
+++ MSStore/src/WebApp/AspnetRunBasics/Startup.cs
@@ -1,3 +1,5 @@
+using AspnetRunBasics.ApiCollection;
+using AspnetRunBasics.ApiCollection.Interfaces;
 using AspnetRunBasics.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -21,6 +23,12 @@ namespace AspnetRunBasics
         {
             services.Configure<ApiSettings>(Configuration.GetSection(nameof(ApiSettings)));
             services.AddSingleton<IApiSettings>(s => s.GetRequiredService<IOptions<ApiSettings>>().Value);
+
+            services.AddHttpClient();
+
+            services.AddTransient<ICatalogApi, CatalogApi>();
+            services.AddTransient<IShopCartApi, ShopCartApi>();
+            services.AddTransient<IOrderApi, OrderApi>();
 
             services.AddRazorPages();
         }
