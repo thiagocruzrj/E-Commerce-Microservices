7e3e4dec88271feaa75fbd1c36d22e400180d277
Thiago Cruz <thagocruz@gmail.com>
2020-08-27 22:14:02 -0300

    Update Index.cshtml.cs

--- MSStore/src/WebApp/AspnetRunBasics/Pages/Index.cshtml.cs
+++ MSStore/src/WebApp/AspnetRunBasics/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AspnetRunBasics.ApiCollection.Interfaces;
+using AspnetRunBasics.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -18,20 +19,31 @@ namespace AspnetRunBasics.Pages
             _shopCartApi = shopCartApi ?? throw new ArgumentNullException(nameof(shopCartApi));
         }
 
-        public IEnumerable<Entities.Product> ProductList { get; set; } = new List<Entities.Product>();
+        public IEnumerable<CatalogModel> ProductList { get; set; } = new List<CatalogModel>();
 
         public async Task<IActionResult> OnGetAsync()
         {
-            ProductList = await _productRepository.GetProducts();
+            ProductList = await _catalogApi.GetCatalog();
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        public async Task<IActionResult> OnPostAddToCartAsync(string productId)
         {
-            //if (!User.Identity.IsAuthenticated)
-            //    return RedirectToPage("./Account/Login", new { area = "Identity" });
+            var product = await _catalogApi.GetCatalogById(productId);
 
-            await _cartRepository.AddItem("test", productId);
+            var userName = "testUser";
+            var shopCart = await _shopCartApi.GetShopCart(userName);
+
+            shopCart.Items.Add(new ShopCartItemModel
+            {
+                ProductId = productId,
+                ProductName = product.Name,
+                Price = product.Price,
+                Quantity = 1,
+                Color = "black"
+            });
+
+            var shopCartUpdated = await _shopCartApi.UpdateShopCart(shopCart);
             return RedirectToPage("Cart");
         }
     }
